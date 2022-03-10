9878a13ca9eaf101614f68d9fb68720a34436897
ersinbulut <ersinblt61@gmail.com>
2022-03-10 12:04:54 +0300

    #34 Asp.Net Core 5.0 Proje Kampı Yazar Kaydı Ve Sweet Alert Kullanımı

--- CoreDemo/Controllers/RegisterController.cs
+++ CoreDemo/Controllers/RegisterController.cs
@@ -1,18 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class RegisterController : Controller
     {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Index()
+        public IActionResult Index(Writer p)
         {
-            return View();
+            p.WriterStatus = true;
+            p.WriterAbout="deneme TEST";
+            wm.WriterAdd(p);
+            return RedirectToAction("Index", "Blog");
         }
     }
 }
